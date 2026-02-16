1a61da6d3ece3e21699e772e82efe9fda1bfb497
Ashni Pothineni <apothine@umich.edu>
2026-02-16 12:09:32 -0500

    Add details for calculations and base code for unit tests

--- p1-starter.py
+++ p1-starter.py
@@ -7,3 +7,37 @@
 # Asked ChatGPT hints for debugging and suggesting the general structure of the code
 # Did your use of GenAI on this assignment align with your goals and guidelines in
 # your Gen AI contract? If not, why?
+
+import unittest
+
+# Calculation 1:
+# For each species, what is the average body mass of penguins that have a bill length greater than 45 mm?
+# Columns used: species, body_mass_g, bill_length_mm
+
+# Calculation 2:
+# How many penguins are there for each sex on each island that have a flipper length greater than 200 mm?
+# Columns used: sex, island, flipper_length_mm
+
+
+# Testing
+class TestCalc(unittest.TestCase):
+    def setUp():
+        # read in subset dataset
+        pass
+
+    def test_calc1(self):
+        pass
+
+    def test_calc1_edge(self):
+        pass
+
+    def test_calc2(self):
+        pass
+
+    def test_calc2_edge(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()

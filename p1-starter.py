c31b1482b046c78103562e62766e64d1a09b0d25
Ashni Pothineni <apothine@umich.edu>
2026-02-19 14:28:42 -0500

    Update subset values and implement all test cases

--- p1-starter.py
+++ p1-starter.py
@@ -10,33 +10,74 @@
 
 import unittest
 
+def read_input(file):
+    pass
+
 # Calculation 1:
 # For each species, what is the average body mass of penguins that have a bill length greater than 45 mm?
 # Columns used: species, body_mass_g, bill_length_mm
+def calc_1(penguins_list):
+    pass
 
 # Calculation 2:
 # How many penguins are there for each sex on each island that have a flipper length greater than 200 mm?
 # Columns used: sex, island, flipper_length_mm
+def calc_2(penguins_list):
+    pass
+
+def write_output():
+    pass
 
 
 # Testing
 class TestCalc(unittest.TestCase):
-    def setUp():
-        # read in subset dataset
-        pass
+    def setUp(self):
+        # read in subset dataset to list of dictionaries
+        self.penguins_list = read_input("penguins_subset.csv")
 
     def test_calc1(self):
-        pass
+        avg_dict = calc_1(self.penguins_list)
+        self.assertEqual(avg_dict["Adelie"], 4175)
+        self.assertEqual(avg_dict["Gentoo"], 4850)
+        self.assertEqual(avg_dict["Chinstrap"], 3475)
 
     def test_calc1_edge(self):
-        pass
+        update_penguins_list = []
+
+        # Removing the only qualifying Gentoo penguin for calc 1 from the list
+        for penguin in self.penguins_list:
+            if not (penguin["species"] == "Gentoo" and penguin["bill_length_mm"] > 45):
+                update_penguins_list.append(penguin)
+
+        avg_dict = calc_1(update_penguins_list)
+        self.assertEqual(avg_dict["Adelie"], 4175)
+        self.assertEqual(avg_dict["Chinstrap"], 3475)
+        self.assertNotIn("Gentoo", avg_dict)    # Gentoo should not exist since no penguins qualify
 
     def test_calc2(self):
-        pass
+        count_dict = calc_2(self.penguins_list)
+        self.assertEqual(count_dict["Torgersen"]["M"], 0)
+        self.assertEqual(count_dict["Torgersen"]["F"], 0)
+        self.assertEqual(count_dict["Dream"]["M"], 1)
+        self.assertEqual(count_dict["Dream"]["F"], 0)
+        self.assertEqual(count_dict["Biscoe"]["M"], 1)
+        self.assertEqual(count_dict["Biscoe"]["F"], 2)
 
     def test_calc2_edge(self):
-        pass
+        update_penguins_list = []
 
+        # Removing the only male penguin on Biscoe for calc 2 from the list
+        for penguin in self.penguins_list:
+            if not (penguin["island"] == "Biscoe" and penguin["flipper_length_mm"] > 200):
+                update_penguins_list.append(penguin)
+
+        count_dict = calc_2(update_penguins_list)
+        self.assertEqual(count_dict["Torgersen"]["M"], 0)
+        self.assertEqual(count_dict["Torgersen"]["F"], 0)
+        self.assertEqual(count_dict["Dream"]["M"], 1)
+        self.assertEqual(count_dict["Dream"]["F"], 0)
+        self.assertEqual(count_dict["Biscoe"]["M"], 0)  # Count should be 0 since no male penguins qualify
+        self.assertEqual(count_dict["Biscoe"]["F"], 2)
 
 
 if __name__ == '__main__':

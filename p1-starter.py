b61d48bd1d665d493ca8a9d6ae065041d65b1103
Ashni Pothineni <apothine@umich.edu>
2026-02-19 22:05:34 -0500

    Implement calc_2() function, update asserts for calc 2 tests to match function logic

--- p1-starter.py
+++ p1-starter.py
@@ -88,11 +88,13 @@ def calc_1(penguins_list):
         bill_length = penguin["bill_length_mm"]
         if bill_length is None:
             continue
-        elif bill_length > 45:
+
+        if bill_length > 45:
             species = penguin["species"]
             body_mass = penguin["body_mass_g"]
             if body_mass is None:
                 continue
+
             if species == 'Adelie':
                 adelie_sum += body_mass
                 a_count += 1
@@ -115,7 +117,31 @@ def calc_2(penguins_list):
     Calculates how many penguins there are for each sex on each island that have a flipper length greater than 200 mm
     Columns used: sex, island, flipper_length_mm
     '''
-    pass
+    counts = {}
+
+    # Loop through each penguin
+        # 1) Get the flipper length of the penguin and check if flipper length is None
+            # 1.5) If true, continue to next penguin
+        # 2) Check if the flipper length is greater than 200 mm
+        # 3) If true, get the island and sex of the penguin
+            # 3.5) If sex is None, continue to next penguin
+        # 4) Get the inner dictionary of count by sex and add to respective count
+    for penguin in penguins_list:
+        flipper_length = penguin["flipper_length_mm"]
+        if flipper_length is None:
+            continue
+        
+        if flipper_length > 200:
+            island = penguin["island"]
+            sex = penguin["sex"]
+            if sex is None:
+                continue
+            
+            s_dict = counts.get(island, {})
+            s_dict[sex] = s_dict.get(sex, 0) + 1
+            counts[island] = s_dict
+
+    return counts
 
 def write_output():
     '''
@@ -148,14 +174,13 @@ class TestCalc(unittest.TestCase):
         avg_dict = calc_1(update_penguins_list)
         self.assertEqual(avg_dict["Adelie"], 4175)
         self.assertEqual(avg_dict["Chinstrap"], 3475)
-        self.assertNotIn(avg_dict["Gentoo"], 0)    # Gentoo should average 0 since no penguins qualify
+        self.assertEqual(avg_dict["Gentoo"], 0)    # Gentoo should average 0 since no penguins qualify
 
     def test_calc2(self):
         count_dict = calc_2(self.penguins_list)
-        self.assertEqual(count_dict["Torgersen"]["male"], 0)
-        self.assertEqual(count_dict["Torgersen"]["female"], 0)
+        self.assertNotIn("Torgersen", count_dict)
         self.assertEqual(count_dict["Dream"]["male"], 1)
-        self.assertEqual(count_dict["Dream"]["female"], 0)
+        self.assertNotIn("female", count_dict["Dream"])
         self.assertEqual(count_dict["Biscoe"]["male"], 1)
         self.assertEqual(count_dict["Biscoe"]["female"], 2)
 
@@ -163,25 +188,30 @@ class TestCalc(unittest.TestCase):
         update_penguins_list = []
 
         # Removing the only male penguin on Biscoe for calc 2 from the list
-            # Other male penguin on Biscoe has exactly 200 mm flipper length (not greater)
-            # Other pengions on Biscoe are 2 females and 1 NA
+            # Other male penguin on Biscoe that has exactly 200 mm flipper length (not greater)
+            # Other penguins on Biscoe are 2 females and 1 NA
         for penguin in self.penguins_list:
-            if not (penguin["island"] == "Biscoe" and penguin["flipper_length_mm"] > 200):
+            if not (penguin["island"] == "Biscoe" and penguin["flipper_length_mm"] > 200 and penguin["sex"] == "male"):
                 update_penguins_list.append(penguin)
 
+        # Check that other male penguin on Biscoe that has exactly 200 mm flipper length still in list
+        self.assertEqual(update_penguins_list[1]["island"], "Biscoe")
+        self.assertEqual(update_penguins_list[1]["flipper_length_mm"], 200)
+        self.assertEqual(update_penguins_list[1]["sex"], "male")
+
         count_dict = calc_2(update_penguins_list)
-        self.assertEqual(count_dict["Torgersen"]["male"], 0)
-        self.assertEqual(count_dict["Torgersen"]["female"], 0)
+        self.assertNotIn("Torgersen", count_dict)
         self.assertEqual(count_dict["Dream"]["male"], 1)
-        self.assertEqual(count_dict["Dream"]["female"], 0)
-        self.assertEqual(count_dict["Biscoe"]["male"], 0)  # Count should be 0 since no male penguins qualify
+        self.assertNotIn("female", count_dict["Dream"])
+        self.assertNotIn("male", count_dict["Biscoe"])  # key of "male" should not exist since no male penguins qualify
         self.assertEqual(count_dict["Biscoe"]["female"], 2)
 
 
 def main():
     penguins_list = read_input('penguins.csv')
     body_mass_avgs = calc_1(penguins_list)
-    # unittest.main()
+    island_sex_count = calc_2(penguins_list)
+    unittest.main()
 
 if __name__ == '__main__':
     main()

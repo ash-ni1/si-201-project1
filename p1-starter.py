0fbc2c7bb64f25df65b1d7535e0566f5f4e99cdb
Ashni Pothineni <apothine@umich.edu>
2026-02-19 22:14:49 -0500

    Update header information

--- p1-starter.py
+++ p1-starter.py
@@ -1,12 +1,13 @@
 # Name: Ashni Pothineni
 # Student ID: 4559 8385
 # Email: apothine@umich.edu
-# Who or what you worked with on this homework (including generative AI like ChatGPT):
+# Who or what you worked with on this homework (including generative AI like ChatGPT): No teammates
 # If you worked with generative AI also add a statement for how you used it.
-# e.g.:
+    # I used Python docs and the lecture slides for help with reading files and writing files
 # Asked ChatGPT hints for debugging and suggesting the general structure of the code
 # Did your use of GenAI on this assignment align with your goals and guidelines in
 # your Gen AI contract? If not, why?
+    # My use of GenAI on this assignment aligned with my goals and guidelines in my GenAI contract.
 
 import csv
 import unittest
